402c915d4473ba2cff4887a12cbf17d90279cd8d
dhanusrimj <114556259+dhanusrimj@users.noreply.github.com>
2022-11-04 17:14:57 +0530

    Add files via upload
    Spring Application

---
+++ java/com/ta/DhanusriApplication.java
@@ -0,0 +1,13 @@
+package com.ta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhanusriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhanusriApplication.class, args);
+	}
+
+}
